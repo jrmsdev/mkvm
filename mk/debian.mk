f66c39b485cd7e649b8f4567b753ac33c24b5c37
Jeremías <jrmsdev@gmail.com>
2017-11-08 13:19:13 -0300

    debian preseed

--- mk/debian.mk
+++ mk/debian.mk
@@ -9,7 +9,7 @@ ISO_MKFS_ARGS += -b isolinux/isolinux.bin -c isolinux/boot.cat
 
 .PHONY: vm-clean
 vm-clean:
-	@rm -vf .vm.* preseed.cfg
+	@rm -vf .vm.*
 
 
 .PHONY: vm-rootfs
@@ -22,24 +22,8 @@ vm-rootfs: .vm.rootfs
 	@echo '>>>'
 	find $(ROOTFS) -type d -exec chmod u+w {} \;
 	find $(ROOTFS) -type f -exec chmod u+w {} \;
-	@$(MAKE) .vm.initrd
-	@touch .vm.rootfs
-
-
-.vm.initrd: preseed.cfg
-#~ 	rm -rf $(WORKDIR)/initrd
-#~ 	mkdir -p $(WORKDIR)/initrd
-#~ 	cd $(WORKDIR)/initrd && ( \
-#~ 		pax -r -z -f ../rootfs/install.amd/initrd.gz; \
-#~ 		cp -v ../preseed.cfg .; \
-#~ 		pax -w -z -f ../rootfs/install.amd/initrd.gz .; \
-#~ 	)
 	cat isolinux.cfg >$(ROOTFS)/isolinux/isolinux.cfg
 	cat preseed.cfg >$(ROOTFS)/preseed.cfg
 	(cd $(ROOTFS) && (rm -f md5sum.txt; \
 		find . -follow -type f | sort -u | xargs md5 -r >md5sum.txt))
-	@touch .vm.initrd
-
-
-preseed.cfg:
-	echo '# preseed!!!' >preseed.cfg
+	@touch .vm.rootfs
