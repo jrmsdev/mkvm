a42ec33639f54bf6c77488970512ad5c799426d2
Jeremías <jrmsdev@gmail.com>
2017-11-08 13:36:50 -0300

    relocate debian common files

--- mk/debian.mk
+++ mk/debian.mk
@@ -5,6 +5,7 @@
 
 ISO_MKFS_ARGS := -r -J -no-emul-boot -boot-load-size 4 -boot-info-table
 ISO_MKFS_ARGS += -b isolinux/isolinux.bin -c isolinux/boot.cat
+FILESDIR := ../files/debian
 
 
 .PHONY: vm-clean
@@ -22,8 +23,18 @@ vm-rootfs: .vm.rootfs
 	@echo '>>>'
 	find $(ROOTFS) -type d -exec chmod u+w {} \;
 	find $(ROOTFS) -type f -exec chmod u+w {} \;
-	cat isolinux.cfg >$(ROOTFS)/isolinux/isolinux.cfg
-	cat preseed.cfg >$(ROOTFS)/preseed.cfg
+	$(MAKE) .vm.preseed .vm.isolinux
 	(cd $(ROOTFS) && (rm -f md5sum.txt; \
 		find . -follow -type f | sort -u | xargs md5 -r >md5sum.txt))
 	@touch .vm.rootfs
+
+
+.vm.isolinux:
+	(preseed_md5=`md5 -r $(ROOTFS)/preseed.cfg`; \
+	cat $(FILESDIR)/isolinux.cfg | \
+		sed "s#\[PRESEED_MD5\]#$${preseed_md5}#" >$(ROOTFS)/isolinux/isolinux.cfg)
+
+
+.vm.preseed:
+	cat $(FILESDIR)/preseed.cfg | \
+		sed 's/\[VM_NAME\]/$(VM_NAME)/' >$(ROOTFS)/preseed.cfg
